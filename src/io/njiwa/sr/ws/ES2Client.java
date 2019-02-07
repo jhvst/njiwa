e59446a21fd6dec89b60a553f02e2fac7c514bce
Paul Bagyenda <bagyenda@dsmagic.com>
2019-02-07 15:14:57 +0300

    - Fixed license and name issues

--- src/io/njiwa/sr/ws/ES2Client.java
+++ src/io/njiwa/sr/ws/ES2Client.java
@@ -1,10 +1,10 @@
 /*
- * Kasuku - Open Source eUICC Remote Subscription Management Server
+ * Njiwa Open Source Embedded M2M UICC Remote Subscription Manager
  * 
  * 
  * Copyright (C) 2019 - , Digital Solutions Ltd. - http://www.dsmagic.com
  *
- * Paul Bagyenda <bagyenda@dsmagic.com>
+ * Njiwa Dev <dev@njiwa.io>
  * 
  * This program is free software, distributed under the terms of
  * the GNU General Public License.
